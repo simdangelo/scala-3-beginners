c65c3fb890f14c5e7122b523de6f83aaa1053107
Simone D'Angelo <sim.dangelo.95@gmail.com>
2023-11-04 22:16:45 +0100

    object-oriented exercise: a linked list

--- src/main/scala/com/rockthejvm/practice/LList.scala
+++ src/main/scala/com/rockthejvm/practice/LList.scala
@@ -1,28 +1,48 @@
 package com.rockthejvm.practice
 
+import scala.annotation.tailrec
+
 // singly linked list
 abstract class LList {
   def head: Int
   def tail: LList
   def isEmpty: Boolean
-  def add(element: Int): LList
+
+  def add(element: Int): LList = new Cons(element, this)
 }
 
-class Empty extends LList{
+class Empty extends LList {
   override def head: Int = throw new NoSuchElementException
   override def tail: LList = throw new NoSuchElementException
   override def isEmpty: Boolean = true
-  override def add(element: Int): LList = new Cons(element, this)
+  override def toString = "[]"
 }
 
-class Cons(override val value: Int, override val tail: LList) extends LList {
+class Cons(override val head: Int, override val tail: LList) extends LList {
   override def isEmpty: Boolean = false
+  override def toString: String = {
+    @tailrec
+    def concatenateElements(remainder: LList, acc: String): String =
+      if (remainder.isEmpty) acc
+      else concatenateElements(remainder.tail, s"$acc, ${remainder.head}")
 
-  override def add(element: Int): LList = new Cons(element, this)
+    s"[${concatenateElements(this.tail, s"$head")}]"
+  }
 }
 
 object LListTest {
   def main(args: Array[String]): Unit = {
+    val empty = new Empty
+    println(empty)
+    println(empty.isEmpty)
+
+    val first3numbers = new Cons(1, new Cons(2, new Cons(3, empty)))
+    println(first3numbers)
+
+    val first3numbers_v2 = empty.add(1).add(2).add(3)
+    println(first3numbers_v2)
+
+    println(first3numbers_v2.isEmpty)
 
   }
 }
