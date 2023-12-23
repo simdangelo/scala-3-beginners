cba2e5e5f068974fadc6bd068aafd76f977c4b9b
Simone D'Angelo <sim.dangelo.95@gmail.com>
2023-12-23 15:42:50 +0100

    modication to singly linked list

--- src/main/scala/com/rockthejvm/practice/LList.scala
+++ src/main/scala/com/rockthejvm/practice/LList.scala
@@ -3,26 +3,25 @@ package com.rockthejvm.practice
 import scala.annotation.tailrec
 
 // singly linked list
-abstract class LList[A] {
-  def head: A
-  def tail: LList[A]
+abstract class LList {
+  def head: Int
+  def tail: LList
   def isEmpty: Boolean
-
-  def add(element: A): LList[A] = new Cons(element, this)
+  def add(element: Int): LList = new Cons(element, this)
 }
 
-class Empty[A] extends LList[A] {
-  override def head: A = throw new NoSuchElementException
-  override def tail: LList[A] = throw new NoSuchElementException
+class Empty extends LList {
+  override def head: Int = throw new NoSuchElementException
+  override def tail: LList = throw new NoSuchElementException
   override def isEmpty: Boolean = true
   override def toString = "[]"
 }
 
-class Cons[A](override val head: A, override val tail: LList[A]) extends LList[A] {
+class Cons(override val head: Int, override val tail: LList) extends LList {
   override def isEmpty: Boolean = false
-  override def toString: String = {
+  override def toString = {
     @tailrec
-    def concatenateElements(remainder: LList[A], acc: String): String =
+    def concatenateElements(remainder: LList, acc: String): String =
       if (remainder.isEmpty) acc
       else concatenateElements(remainder.tail, s"$acc, ${remainder.head}")
 
@@ -32,19 +31,19 @@ class Cons[A](override val head: A, override val tail: LList[A]) extends LList[A
 
 object LListTest {
   def main(args: Array[String]): Unit = {
-    val empty = new Empty[Int]
-    println(empty)
+
+    // EMPTY LIST
+    val empty = new Empty
     println(empty.isEmpty)
+    println(empty)
 
-    val first3numbers = new Cons(1, new Cons(2, new Cons(3, empty)))
-    println(first3numbers)
+    // NON-EMPTY LIST - solution1
+    val first3Numbers = new Cons(1, new Cons(2, new Cons(3, empty)))
+    println(first3Numbers)
 
-    val first3numbers_v2 = empty.add(1).add(2).add(3)
-    println(first3numbers_v2)
-    println(first3numbers_v2.isEmpty)
-
-    val someStrings = new Cons("dog", new Cons("cat", new Empty))
-    println(someStrings)
-
+    // NON-EMPTY LIST - solution2
+    val first3Numbers_v2 = empty.add(1).add(2).add(3)
+    println(first3Numbers_v2)
+    println(first3Numbers_v2.isEmpty)
   }
 }
